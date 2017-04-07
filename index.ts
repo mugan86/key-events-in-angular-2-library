8de375e63d332ead388f1fa6be1091bc64883c64
Anartz Mugika <mugan86@gmail.com>
2017-04-07 14:30:36 +0200

    Change KeyboardsEventsModule instead of SampleModule

--- index.ts
+++ index.ts
@@ -1,35 +1,25 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KeyboardEventsComponent } from './src/keyboard-events.component';
-import { SampleDirective } from './src/sample.directive';
-import { SamplePipe } from './src/sample.pipe';
-import { SampleService } from './src/sample.service';
 
 export * from './src/keyboard-events.component';
-export * from './src/sample.directive';
-export * from './src/sample.pipe';
-export * from './src/sample.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    KeyboardEventsComponent,
-    SampleDirective,
-    SamplePipe
+    KeyboardEventsComponent
   ],
   exports: [
-    KeyboardEventsComponent,
-    SampleDirective,
-    SamplePipe
+    KeyboardEventsComponent
   ]
 })
-export class SampleModule {
+export class KeyboardsEventsModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: SampleModule,
-      providers: [SampleService]
+      ngModule: KeyboardsEventsModule,
+      providers: []
     };
   }
 }
