8ce4e3d0d25ec1d7b34958b5cabfc99d3e721b23
Anartz Mugika <mugan86@gmail.com>
2017-04-07 11:36:31 +0200

    Add new import references

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SampleComponent } from './src/sample.component';
+import { KeyboardEvents } from './src/keyboard-events.component';
 import { SampleDirective } from './src/sample.directive';
 import { SamplePipe } from './src/sample.pipe';
 import { SampleService } from './src/sample.service';
 
-export * from './src/sample.component';
+export * from './src/keyboard-events.component';
 export * from './src/sample.directive';
 export * from './src/sample.pipe';
 export * from './src/sample.service';
@@ -15,12 +15,12 @@ export * from './src/sample.service';
     CommonModule
   ],
   declarations: [
-    SampleComponent,
+    KeyboardEvents,
     SampleDirective,
     SamplePipe
   ],
   exports: [
-    SampleComponent,
+    KeyboardEvents,
     SampleDirective,
     SamplePipe
   ]
